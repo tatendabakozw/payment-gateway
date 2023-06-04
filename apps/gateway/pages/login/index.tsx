a321e10160f8d3fa244e05e0e00fdc7ee8ebd15b
tatendabakozw <tatendabakozw@gmail.com>
2023-06-04 14:24:26 +0200

    feature: added padding on auth page

--- apps/gateway/pages/login/index.tsx
+++ apps/gateway/pages/login/index.tsx
@@ -29,7 +29,7 @@ export function Login(props: LoginProps) {
   };
 
   return (
-    <div className="overflow-hidden min-h-screen grid items-center  w-full">
+    <div className="overflow-hidden relative min-h-screen grid items-center  w-full px-4">
       <div className="z-0 absolute top-40 left-4 md:w-96 w-60 md:h-96 h-60 bg-yellow-200 rounded-full blur-3xl opacity-50 mix-blend-multiply animate-blob" />
       <div className="z-0 absolute top-52 right-52 md:w-96 w-60 md:h-96 h-60 bg-purple-200 rounded-full blur-3xl opacity-50 mix-blend-multiply animate-blob animation-delay-2000" />
       <div className="z-0 absolute bottom-8 left-80 md:w-96 w-60 md:h-96 h-60 bg-pink-200 rounded-full blur-3xl opacity-50 mix-blend-multiply animate-blob animation-delay-4000" />
