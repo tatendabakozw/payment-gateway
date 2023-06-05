4644ad011c09bd4022168b86264c93923d4d6850
tatendabakozw <tatendabakozw@gmail.com>
2023-06-05 09:50:11 +0200

    feature: added dashboard app

---
+++ apps/dashboard/pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from './index.module.css';
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+ <div className="flex bg-green-200 p-4 w-full">
+  Dashboard Here
+ </div>
+  );
+}
+
+export default Index;
